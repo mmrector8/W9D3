b0f3d85fe99f77b2912a8c1d7646716313e8bf9b
Morgan Rector <mmrector@yahoo.com>
2022-11-30 15:40:33 -0800

    Update tictactoe

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -5,6 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
   // Your code here
   let newGame = new Game();
   let figure = document.querySelector(".ttt")
-  let newView = new View(newGame, figure)
-  newView.setupBoard();
+  //let newView = new View(newGame, figure)
+
+  let ul = document.querySelector("#grid")
+  console.log(ul)
+  ul.addEventListener("click", handleClick)
+
 });
--- tictactoe/src/ttt-view.js
+++ tictactoe/src/ttt-view.js
@@ -4,26 +4,32 @@ class View {
   constructor(game, el) {
     this.game = game;
     this.el = el;
+    this.setupBoard();
   }
 
   setupBoard() {
     let grid = document.getElementById("grid")
     // create 3x3 grid, 9 li elements needed
-    let idx=0, count=0;
     for(let i=0; i < 3; i++){
       for (let j=0; j < 3; j++) {
         let pos = [i, j]
         let newLi = document.createElement("li")
         newLi.dataset.indexNumber = pos;
-        newLi.innerHTML = "X"
         grid.appendChild(newLi);
       }
     }
   }
   
-  // bindEvents() {}
+  bindEvents() {
 
-  // handleClick(e) {}
+  }
+
+  handleClick(e) {
+    e.stopPropagation();
+    let clickedLi = Array.from(e.target.data-index-number)
+    alert("clicked!")
+    this.game.playMove(clickedLi);
+  }
 
   // makeMove(square) {}
 
